ac60d9fa1ae176c342286c18d9e5bf657ec72527
situki <58246408+situki@users.noreply.github.com>
2023-01-27 10:24:23 +0900

    Update EnumCreater.cs

--- LoveBullet/Assets/Scripts/Utility/EnumCreater.cs
+++ LoveBullet/Assets/Scripts/Utility/EnumCreater.cs
@@ -6,34 +6,34 @@ using UnityEditor;
 
 namespace Utility
 {
+    [System.Serializable]
+    public class EnumState
+    {
+        public string name;
+        public int num;
+    }
     /// <summary>
     /// 配列からEnumを生成するスクリプト
     /// </summary>
     public class EnumCreater
     {
-        [System.Serializable]
-        public class EnumState
-        {
-            public string name;
-            public int num;
-        }
 
 #if UNITY_EDITOR
-        public static void CreateEnumCs(string _csName,string _enumName,List<EnumState> _state)
+        public static void CreateEnumCs(string _csName, string _enumName, List<EnumState> _state)
         {
             // 作成するアセットのパス
-            var filePath = "Assets/Scripts/Enum/" +  _csName +".cs";
+            var filePath = "Assets/Scripts/Enum/" + _csName + ".cs";
 
             string code;
 
             code = "public enum " + _enumName + "{\n";
 
             foreach (var s in _state) {
-                code +="    " + s.name +" = " + s.num.ToString() +  ",\n";
+                code += "    " + s.name + " = " + s.num.ToString() + ",\n";
             }
 
 
-            
+
             code += "    Max \n}";
 
             // アセット(.cs)を作成する
