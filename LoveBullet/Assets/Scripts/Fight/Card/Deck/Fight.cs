cd185998ac2e29612290aa3b661d4371caba5205
situki <58246408+situki@users.noreply.github.com>
2023-02-13 18:54:55 +0900

    Update Fight.cs

--- LoveBullet/Assets/Scripts/Fight/Card/Deck/Fight.cs
+++ LoveBullet/Assets/Scripts/Fight/Card/Deck/Fight.cs
@@ -25,7 +25,7 @@ namespace Card
 
         // デバッグ用の銃内のカード表示処理
 #if UNITY_EDITOR
-        [SerializeField] List<Card.State> GunInCard;
+        [SerializeField]List<Card.State> GunInCard;
         private void FixedUpdate()
         {
             GunInCard = gunInCards.ToList();
@@ -41,7 +41,7 @@ namespace Card
         [SerializeField] int explanationCardNum = 0;
 
         [Header("その他ルール系")]
-        [SerializeField, ReadOnly] int floor;
+        [SerializeField,ReadOnly] int floor;
         [SerializeField] int reloadCost = 1;
         [SerializeField] int cockingCost = 1;
         [SerializeField, ReadOnly] bool playerTurn = true;
@@ -53,7 +53,7 @@ namespace Card
         public GameObject enemyBase;
         List<Enemy.Enemy.State> enemysState = new List<Enemy.Enemy.State>();
         public List<Enemy.Enemy> enemyObjects = new List<Enemy.Enemy>();
-        [SerializeField, ReadOnly] int targetId = 0;
+        [SerializeField,ReadOnly]int targetId = 0;
         public int TargetId => targetId;
 
         public ReactiveCollection<Enemy.Enemy> actEnemy = new ReactiveCollection<Enemy.Enemy>();
@@ -62,7 +62,8 @@ namespace Card
 
         private void Awake()
         {
-            if (SingletonCheck(this, true)) {
+            if (SingletonCheck(this,true))
+            {
             }
         }
         void Start()
@@ -92,7 +93,8 @@ namespace Card
         {
 
 
-            if (enemyObjects.Count == 0 && ResultManager.instance.isResult == false) {
+            if (enemyObjects.Count == 0 && ResultManager.instance.isResult == false)
+            {
                 //TODO仮置き
                 ResultManager.instance.StartResult(15, true);
             }
@@ -112,12 +114,15 @@ namespace Card
         void StartTurn_Enemy()
         {
             //エネミー側で削除されていた場合リストから削除しておく
-            for (int i = 0; i < enemyObjects.Count; i++) {
-                if (enemyObjects[i] == null) {
+            for(int i=0;i< enemyObjects.Count; i++)
+            {
+                if (enemyObjects[i] == null)
+                {
                     enemyObjects.RemoveAt(i);
                 }
             }
-            foreach (var enemy in enemyObjects) {
+            foreach (var enemy in enemyObjects)
+            {
                 enemy.Action();//敵に順番に行動させる
                 enemy.ResetDF();//DFをリセットする
             }
@@ -158,7 +163,8 @@ namespace Card
 
             // 攻撃回数分ループする
             var attackNum = gunInCards[0].MultiAttack;
-            if (gunInCards[0].Damage > 0 && attackNum == 0) {
+            if(gunInCards[0].Damage>0&& attackNum == 0)
+            {
                 attackNum++;
             }
             for (int i = 0; i < attackNum; i++) {
@@ -196,7 +202,7 @@ namespace Card
 
             // 強制リロードするか
             if (_card.Reload > 0) {
-                for (int i = _card.Reload; i > 0; i--) {
+                for(int i= _card.Reload; i > 0; i--) {
                     Reload();
                 }
             }
@@ -215,7 +221,7 @@ namespace Card
             //プレイヤーの攻撃アニメーション
             Player.instance.AttackAnim();
         }
-
+        
         /// <summary>
         /// ターンを進行する
         /// </summary>
@@ -223,7 +229,7 @@ namespace Card
         public void ProgressTurn(int _progressTurn)
         {
             bool _flg = true;
-            foreach (var _enemy in enemyObjects) {
+            foreach(var _enemy in enemyObjects) {
                 if (_enemy.ProgressTurn(_progressTurn)) {
                     _flg = playerTurn = false;
                     actEnemy.Add(_enemy);
@@ -261,7 +267,7 @@ namespace Card
         /// </summary>
         /// <param name="_atk"></param>
         /// <param name="_def"></param>
-        public void ReceiveWeaken(int _atk, int _def)
+        public void ReceiveWeaken(int _atk,int _def)
         {
             plState.ATWeaken.Value += _atk;
             plState.DFWeaken.Value += _def;
@@ -298,7 +304,8 @@ namespace Card
         void InitializeStartDeck()
         {
 
-            foreach (var cardId in startDecksId) {
+            foreach (var cardId in startDecksId)
+            {
                 deckList.Add(Search.GetCard(cardId));
             }
         }
@@ -326,7 +333,8 @@ namespace Card
         public void Reload(int bulletNum = 6)
         {
             //リボルバー内を空に 中身があるならTrushに移動させる
-            foreach (var card in gunInCards) {
+            foreach(var card in gunInCards)
+            {
                 if (card.id != 0) trashInCards.Add(card);
             }
             gunInCards.Clear();
@@ -336,12 +344,14 @@ namespace Card
 
 
             //デッキ内カードが込める弾の個数より少ない時
-            if (deckInCards.Count < bulletNum) {
+            if (deckInCards.Count < bulletNum)
+            {
                 //山札がないので捨て札をシャッフルして山札に加える
                 ResetTrush();
             }
             //デッキからカード追加
-            for (int i = 0; i < bulletNum; i++) {
+            for (int i = 0; i < bulletNum; i++)
+            {
                 gunInCards.Add(deckInCards[0]);
                 deckInCards.RemoveAt(0);
             }
@@ -373,8 +383,8 @@ namespace Card
         {
             // トラッシュリストをシャッフルしてデッキに入れる
             trashInCards = trashInCards.OrderBy(a => Guid.NewGuid()).ToList();
-
-            foreach (var _cards in trashInCards) {
+            
+            foreach(var _cards in trashInCards) {
                 deckInCards.Add(_cards);
             }
 
@@ -411,8 +421,9 @@ namespace Card
             var enemyGroup = Enemy.AddventPattern.GetGroup(floor, rand);
 
             int enemyCount = 0;
-            foreach (var enemyId in enemyGroup) {
-
+            foreach (var enemyId in enemyGroup)
+            {
+               
                 if (enemyId == -1) continue;
                 enemysState.Add(Enemy.Enemy.GetEnemyState(enemyId));
                 //敵生成
